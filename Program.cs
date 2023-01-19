2d8290378aa38555b12148515019cbd7dd1ca7a6
Mitch Anderson <mitch921@byu.edu>
2023-01-18 19:35:20 -0700

    Added Comments

--- Program.cs
+++ Program.cs
@@ -9,13 +9,15 @@ namespace Mission2Assignment
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //Intro/prompt
             Console.WriteLine("Welcome to the dice throwing simulator!\n\nHow many dice rolls would you like to simulate?");
 
+            //initialize array and collect number of rolls
             int numRolls = Convert.ToInt32(Console.ReadLine());
             int[] rollResults = new int[13];
             Random r = new Random();
 
+            //Roll Dice and increment array based on total
             for (int i = 0; i < numRolls; i++)
             {
                 int firstDie = r.Next(1,7);
@@ -24,15 +26,17 @@ namespace Mission2Assignment
 
                 rollResults[rollTotal]++;
             }
-
+            //Begin output
             string output = "DICE ROLLING SIMULATION RESULTS \nEach \"*\" represents 1 % of the total number of rolls.\nTotal number of rolls = " + Convert.ToString(numRolls) + ".\n\n";
             
-
+            //Make output based on totals of each number rolled
             for (int i = 2; i < 13; i++)
             {
                 double pctRolled = ((double)rollResults[i] / numRolls) * 100;
                 int stars = (int)Math.Round(pctRolled);
                 string starOutput = "";
+                //Sets number of * for each number rolled
+
                 for (int iCount = 0; iCount < stars; iCount++)
                 {
                     starOutput += "*";
@@ -41,6 +45,8 @@ namespace Mission2Assignment
                 output += (Convert.ToString(i)) + ": " + starOutput + "\n";
                 
             }
+
+            //Final output
             output += "\nThank you for using the dice throwing simulator. Goodbye!";
             Console.WriteLine(output);
 
